443bd70adb8c0e7febe9433e20328c294744be26
alimrizvi <62912143+alimrizvi@users.noreply.github.com>
2025-06-18 14:45:40 +0500

    Build the basic arithmetic functions and the operator function

---
+++ script.js
@@ -0,0 +1,36 @@
+const add = function(num1,num2) {
+    return num1 + num2
+}
+
+const subtract = function(num1,num2) {
+    return num1 - num2
+}
+
+const multiply = function(num1,num2) {
+    return num1 * num2
+}
+
+const divide = function(num1,num2) {
+    return num1 / num2
+}
+
+const operate = function(num1,operator,num2) {
+    if (operator=='+') {
+        return add(num1,num2)
+    }
+    else if (operator=='-') {
+        return subtract(num1,num2)
+    }
+    else if (operator=='*') {
+        return multiply(num1,num2)
+    }
+    else if (operator=='/') {
+        return divide(num1,num2)
+    }
+    else {return "Error"}
+}
+
+console.log(operate(4,'+',2))
+console.log(operate(4,'-',2))
+console.log(operate(4,'*',2))
+console.log(operate(4,'/',2))

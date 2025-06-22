ce4a18795fd34a531683bf13083f392600126464
alimrizvi <62912143+alimrizvi@users.noreply.github.com>
2025-06-23 01:45:09 +0500

    Calculator can now evaluate correctly typed expressions

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const add = function(num1,num2) {
-    return num1 + num2
+    return +num1 + +num2
 }
 
 const subtract = function(num1,num2) {
-    return num1 - num2
+    return +num1 - +num2
 }
 
 const multiply = function(num1,num2) {
-    return num1 * num2
+    return +num1 * +num2
 }
 
 const divide = function(num1,num2) {
-    return num1 / num2
+    return +num1 / +num2
 }
 
 const operate = function(num1,operator,num2) {
@@ -39,12 +39,81 @@ const createButtonsFromList = function(buttonList,buttonContainer,clickFunction)
     }
 }
 
+const clearScreen = function() {
+    exprssionDisplay.textContent = ''
+    resultDisplay.textContent = ''
+}
+
+const delineateExpressionArray = function(expressionArray,operator) {
+    let resultArray = []
+    for (let n=0;n<expressionArray.length;n++) {
+        splitArray = expressionArray[n].split(operator)
+        let splitIndex = 1
+        for (let i=0;i<splitArray.length;i++) {
+            if (splitIndex<splitArray.length) {
+                splitArray.splice(splitIndex,0,operator)
+            }
+            splitIndex += 2
+        }
+        resultArray.push(splitArray)
+    }
+    return resultArray.flat()
+}
+
+const runArithemeticOperation = function(expressionArray,operator) {
+    let operatorIndex = expressionArray.indexOf(operator)
+    if (operatorIndex==-1) {
+        return false
+    }
+    else {
+        // console.log(expressionArray[operatorIndex-1],expressionArray[operatorIndex],expressionArray[operatorIndex+1])
+        let answer = operate(expressionArray[operatorIndex-1],expressionArray[operatorIndex],expressionArray[operatorIndex+1])
+        expressionArray.splice(operatorIndex-1,3,answer)
+        return true
+    }
+}
+
+const solveExpression = function(expressionArray) {
+    // console.log(expressionArray)
+    if (expressionArray.length==1) {
+        return true
+    }
+    else {
+        for (let i=0;i<arithimeticOperators.length;i++) {
+            let operator = arithimeticOperators[i]
+            let operationRequired = true
+            while (operationRequired) {
+                operationRequired = runArithemeticOperation(expressionArray,operator)
+            }
+            
+        }
+        solveExpression(expressionArray)
+    }
+        
+    // console.log(expressionArray)
+}
+
+const evaluateExpression = function() {
+    let expressionArray = new Array(exprssionDisplay.textContent)
+    for (let i=0;i<arithimeticOperators.length;i++) {
+        operator = arithimeticOperators[i]
+        // console.log(operator)
+        expressionArray = delineateExpressionArray(expressionArray,operator)
+    }
+    solveExpression(expressionArray)
+    resultDisplay.textContent = expressionArray[0]
+    // console.log(expressionArray)
+}
+
+
+
 const runSpecialOperation = function(e) {
     const choice = e.target.textContent
-    console.log(choice)
     if (choice =='AC') {
-        exprssionDisplay.textContent = ' '
-        resultDisplay.textContent = ' '
+        clearScreen()
+    }
+    else if (choice =='=') {
+        evaluateExpression()
     }
 }
 
@@ -58,12 +127,10 @@ const resultDisplayContainer = document.querySelector('#result-display')
 
 exprssionDisplay = document.createElement('span')
 resultDisplay = document.createElement('span')
-exprssionDisplay.textContent = '5+4'
-resultDisplay.textContent =  '9'
 expressionDisplayContainer.appendChild(exprssionDisplay)
 resultDisplayContainer.appendChild(resultDisplay)
 
-const arithimeticOperators = ['+','-','*','/']
+const arithimeticOperators = ['/','*','+','-']
 const specialOperators = ['AC','=','+/-','.']
 const numList = [0,1,2,3,4,5,6,7,8,9]
 const numberButtonsContainer = document.querySelector('#number-buttons')
